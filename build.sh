092a35ad5bd0079d890623ce6989a6f2cbec8e20
Opitz <opitz@Opitzs-MacBook-Air.local>
2024-11-11 23:21:31 -0700

    Added build script for Netlify

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download the .NET install script
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+
+# Make the script executable
+chmod +x ./dotnet-install.sh
+
+# Install the .NET SDK for .NET 8.0
+./dotnet-install.sh --channel 8.0
+
+# Export environment variables so that dotnet command is available
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$HOME/.dotnet:$HOME/.dotnet/tools
+
+# Build the project
+dotnet publish SimpleBlazor/SimpleBlazor.csproj -c Release -o build
